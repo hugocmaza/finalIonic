fe0284e7b51887e26acea0ba59d701d7d6146c5d
hmazaITP <hmaza@itpatagonia.com>
2023-12-13 19:31:02 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'finalIonic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
